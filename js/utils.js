713b78fff348e27646e9db7de8790049a8979745
Blakkis <greatshader@hotmail.com>
2020-08-10 10:45:01 +0300

    Update

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,10 @@
 function updateLastEdit()
 {
 	var last_edit = document.getElementById("j_last_edit");
+	if (last_edit === null){
+		return;
+	}
+
 	last_edit.innerHTML = "Last Update: " + document.lastModified;
 }
 
